5ec00b94326da651838ac49ff467ce3b153a9187
이수진 <slrkajsep337@naver.com>
2022-11-15 14:17:05 +0900

    객체 생성

--- src/main/java/com/mustache/bbshospital/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbshospital/repository/HospitalRepository.java
@@ -4,5 +4,18 @@ import com.mustache.bbshospital.domain.entity.Hospital;
 import com.mustache.bbshospital.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+    List<Hospital> findByRoadNameAddressContaining(String roadNameAddresses); // 포함
+    List<Hospital> findByRoadNameAddressStartsWith(String roadNameAddresses); // 시작
+    List<Hospital> findByHospitalNameEndsWith(String roadNameAddresses); // 끝남
+    List<Hospital> findByPatientRoomCountGreaterThanAndPatientRoomCountLessThan(int var1, int var2);
+
+
+
+
+
+
 }
