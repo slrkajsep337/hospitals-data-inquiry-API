194441867d83078c8bd21a3b97ea23bc693c425e
이수진 <slrkajsep337@naver.com>
2022-11-21 23:59:59 +0900

    service 생성

--- src/main/java/com/mustache/bbshospital/service/HospitalService.java
+++ src/main/java/com/mustache/bbshospital/service/HospitalService.java
@@ -1,5 +1,6 @@
 package com.mustache.bbshospital.service;
 
+import com.mustache.bbshospital.domain.dto.HospitalResponse;
 import com.mustache.bbshospital.domain.entity.Hospital;
 import com.mustache.bbshospital.repository.HospitalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class HospitalService {
@@ -18,6 +20,20 @@ public class HospitalService {
     @Transactional
     public Page<Hospital> getHospitalList(Pageable pageable) {
         return hr.findAll(pageable);
+    }
+
+    public HospitalResponse getHospital(Integer id) {
+        Optional<Hospital> optHospital = hr.findById(id); // Entity
+        Hospital hospital = optHospital.get();
+        HospitalResponse hospitalResponse = Hospital.of(hospital); // DTO
+        if (hospital.getBusinessStatusCode() == 13) {
+            hospitalResponse.setBusinessStatusName("영업중");
+        } else if (hospital.getBusinessStatusCode() == 3) {
+            hospitalResponse.setBusinessStatusName("폐업");
+        } else {
+            hospitalResponse.setBusinessStatusName(String.valueOf(hospital.getBusinessStatusCode()));
+        }
+        return hospitalResponse;
     }
 }
 
