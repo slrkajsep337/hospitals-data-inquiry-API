feee52eb35452060671a78bc7bd53f89d445b00e
이수진 <slrkajsep337@naver.com>
2022-11-21 23:57:59 +0900

    생성자 추가

--- src/main/java/com/mustache/bbshospital/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbshospital/domain/dto/HospitalResponse.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class HospitalResponse {
+    //json으로 출력해줄 내용
     private Integer id;
     private String roadNameAddress;
     private String hospitalName;
@@ -17,6 +18,24 @@ public class HospitalResponse {
     private Integer totalNumberOfBeds;
     private String businessTypeName;
     private Float totalAreaSize;
+    private String businessStatusName;
+
+    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount,
+                            Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.totalAreaSize = totalAreaSize;
+    }
+
+    public void setBusinessStatusName(String businessStatusName) {
+        this.businessStatusName = businessStatusName;
+    }
+
+
 
 }
 
